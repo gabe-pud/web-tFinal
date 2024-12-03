7901716ac57b7eda4bbd801d25e738d6fe88ae20
gabe-pud <gabrielgarciasantana3@gmail.com>
2024-12-02 21:58:09 -0300

    ajustes de estilização

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+//nav
 const hamburger = document.querySelector(".hamburger");
 
 const nav = document.querySelector(".nav");
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
 
-// carroussel
+
+
